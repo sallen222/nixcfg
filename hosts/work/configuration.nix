b8b8d84a6228beb16d0b9a51414e64b8704c9e45
sallen6 <s.allen@elsevier.com>
2024-05-17 17:42:14 +0000

    Changes to config

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -1,27 +1,26 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-# NixOS-WSL specific options are documented on the NixOS-WSL repository:
-# https://github.com/nix-community/NixOS-WSL
-
-{ config, lib, pkgs, ... }:
-
+{ config, pkgs, inputs, ... };
 {
-  imports = [
-    # include NixOS-WSL modules
-    <nixos-wsl/modules>
+  imports = with inputs.self.nixosModules; [
+    users-steveallen
+    editor-nvim
+  ];
+  
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users = import "${inputs.self}/users";
+    extraSpecialArgs = {
+      inherit inputs;
+    }
+  };
+
+  time.timeZone = "America/Philadelphia";
+
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    git
   ];
 
-  wsl.enable = true;
-  wsl.defaultUser = "steve";
-  
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11"
 }
