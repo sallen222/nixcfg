e27ecafde68d60b2f5933b27ecc1de6bcd3621d1
sallen6 <s.allen@elsevier.com>
2024-05-20 18:15:57 +0000

    Add completion to neovim

--- users/steve/modules/nixvim.nix
+++ users/steve/modules/nixvim.nix
@@ -3,6 +3,37 @@
 {
   programs.nixvim = {
     enable = true;
+
     colorschemes.gruvbox.enable = true;
+
+    plugins = {
+      lualine.enable = true;
+    };
+
+    plugins.lsp = {
+      enable = true;
+      servers = { 
+	# js / ts
+        tsserver.enable = true;
+      };
+    };
+
+    plugins.cmp = {
+      enable = true;
+      settings.sources = [
+        {
+          name = "nvim_lsp";
+	  groupIndex = 1;
+        }
+        {
+          name = "path";
+	  groupIndex = 1;
+        }
+        {
+    	  name = "buffer";
+          groupIndex = 2;
+        }
+      ];
+    };
   };
 }
